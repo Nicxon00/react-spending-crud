e3ae6b0ed2d18e33d49915b73e0bd87452de285c
Nicxon <nicxon00@gmail.com>
2023-09-28 08:45:27 +0700

    push to github

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const element1 = <h1>waduh!</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element1);
